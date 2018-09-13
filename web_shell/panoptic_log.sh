d5f487be5bf1e9c21618264b45b6f4b23153c64e
matheusam <38408536+matheusam@users.noreply.github.com>
2018-09-13 13:27:30 -0300

    Update panoptic_log.sh

--- web_shell/panoptic_log.sh
+++ web_shell/panoptic_log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/Panoptic/;
-python panoptic.py --cookie="VISITOR_INFO1_LIVE=YtUTSTzBa_c; PREF=f1=50000000; YSC=RQuY639cxEo" -u http://admig.com/novidades_pt.php?id=1;
+python panoptic.py --cookie="VISITOR_INFO1_LIVE=YtUTSTzBa_c; PREF=f1=50000000; YSC=RQuY639cxEo" -u http://admin.com/novidades_pt.php?id=1;
